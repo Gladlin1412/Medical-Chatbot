e8bcc39e1ae4e3d3b79870d80943db66a79ee789
Gladlin1412 <gladlinmascarenhas14@gmail.com>
2025-12-30 14:23:27 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Gladlin Mascarenhas",
+    author_email="22a47.gladlin@sjec.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
